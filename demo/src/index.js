7a18403d21751b3efd84cbf11a737dc14d422fcf
xusihan <383688501@qq.com>
2018-05-22 17:58:18 +0800

    development

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,19 +1,13 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print';
 
 function component() {
     var element = document.createElement('div');
-    element.innerHTML = _.join(['hello', 'webpack', '例子'], ' ');
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
+    element.innerHTML = _.join(['hello', 'webpack', '例zz'], ' ');
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console.';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 };
 
